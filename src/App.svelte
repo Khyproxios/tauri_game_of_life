209c19f061eca3bfb47d9c556be2d37fa81bb0fe
Damien Bevins <damien.bevins@outlook.com>
2023-12-28 23:01:33 -0500

    Cull some more boilerplate stuff

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,10 @@
 </script>
 
 <main class="container">
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
   <div class="row">
     <Counter />
   </div>
-
 </main>
 
 <style>
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
 </style>
